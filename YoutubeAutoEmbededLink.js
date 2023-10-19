ca361bf4f4cfe6813de7eca3d45d359423b8bf39
TheClockworkVoid <148404938+TheClockworkVoid@users.noreply.github.com>
2023-10-19 03:45:11 +0200

    Update YoutubeAutoEmbededLink.js
    fixed typo in match attribute causing the plugin not actually triggering

--- YoutubeAutoEmbededLink.js
+++ YoutubeAutoEmbededLink.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  Vive la résistance!
 // @author       TheClockworkVoid
-// @match        https://www.youtube.com*
+// @match        https://www.youtube.com/*
 // ==/UserScript==
 
 (function () {
